841c05f1245576f18efc6483f51914d33a230ffa
Arthur Mensch <arthur.mensch@gmail.com>
2015-05-30 11:54:52 +0200

    Added inline comment

--- sklearn/utils/testing.py
+++ sklearn/utils/testing.py
@@ -709,7 +709,7 @@ def _delete_folder(folder_path, warn=False):
     Copy from joblib.pool (for independance)"""
     try:
         if os.path.exists(folder_path):
-            shutil.rmtree(folder_path)
+            shutil.rmtree(folder_path)  # This can fail under windows, but will succeed when called by atexit
     except shutil.WindowsError:
         if warn:
             warnings.warn("Could not delete temporary folder %s" % folder_path)
