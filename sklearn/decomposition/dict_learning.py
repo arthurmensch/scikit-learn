16048ce00b09b8ac8245f5eb391bc743260c9f67
Arthur Mensch <arthur.mensch@gmail.com>
2015-06-25 15:20:48 +0200

    Commit before demonstration of threading failure

--- sklearn/decomposition/dict_learning.py
+++ sklearn/decomposition/dict_learning.py
@@ -261,14 +261,14 @@ def sparse_encode(X, dictionary, gram=None, cov=None, algorithm='lasso_lars',
 
     if pool is not None and algorithm is 'lasso_cd':
         slices = list(gen_even_slices(n_samples, pool._processes))
-        code_views = pool.map(lambda this_slice_: _sparse_encode(X[this_slice_], dictionary, gram, cov[:, this_slice_], algorithm,
+        code_views = pool.map(lambda this_slice_: _sparse_encode(X[this_slice_].copy(), dictionary, gram, cov[:, this_slice_].copy(), algorithm,
                                                     regularization=regularization, copy_cov=copy_cov,
-                                                    init=init[this_slice_] if init is not None else None,
+                                                    init=init[this_slice_].copy() if init is not None else None,
                                                     max_iter=max_iter),
                               slices)
     else:
         slices = list(gen_even_slices(n_samples, _get_n_jobs(n_jobs)))
-        code_views = Parallel(n_jobs=n_jobs, backend='threading')(
+        code_views = Parallel(n_jobs=n_jobs, backend='threading' if algorithm == 'lasso_cd' else 'multiprocessing')(
             delayed(_sparse_encode)(
                 X[this_slice], dictionary, gram, cov[:, this_slice], algorithm,
                 regularization=regularization, copy_cov=copy_cov,
