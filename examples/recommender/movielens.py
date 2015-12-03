bbf4dfd76d6b99da2a92685e7ce24d4201c18d6f
Arthur Mensch <arthur.mensch@m4x.org>
2015-12-03 09:14:21 +0100

    Drago

--- examples/recommender/movielens.py
+++ examples/recommender/movielens.py
@@ -555,7 +555,7 @@ def run(n_jobs=1):
     mem = Memory(cachedir=expanduser("~/cache"), verbose=10)
     print("Loading dataset")
     X = mem.cache(fetch_ml_10m)(expanduser('~/data/own/ml-10M100K'),
-                                remove_empty=True, n_users=1000)
+                                remove_empty=True, n_users=10000)
     X = X[random_state.permutation(X.shape[0])]
     print("Done loading dataset")
     splits = list(CsrRowStratifiedShuffleSplit(X, test_size=0.1, n_splits=1,
@@ -580,8 +580,8 @@ def run(n_jobs=1):
                                     random_state=random_state)
                     for n_components in [50]
                     for batch_size in [10]
-                    for l1_ratio in [0., 0.5, 1.]
-                    for alpha in np.logspace(-2, 2, 5)]
+                    for l1_ratio in [1.]
+                    for alpha in [1]]
     # recommenders = [SPCARecommender(n_components=20,
     #                                 batch_size=1,
     #                                 alpha=0.1,
@@ -598,5 +598,5 @@ def run(n_jobs=1):
 
 
 if __name__ == '__main__':
-    run(n_jobs=24)
+    run(n_jobs=1)
     # gather_results(expanduser('~/output/movielens/2015-12-02_14-38-20'))
