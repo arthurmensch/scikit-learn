9d1a3ec45853eae5690f71836ba878c222c7e6ef
Arthur Mensch <arthur.mensch@m4x.org>
2015-12-09 11:22:30 +0100

    Initial arrayfuncs addition

--- sklearn/utils/arrayfuncs.pyx
+++ sklearn/utils/arrayfuncs.pyx
@@ -4,6 +4,7 @@ Small collection of auxiliary functions that operate on arrays
 """
 cimport numpy as np
 import  numpy as np
+from cpython.array cimport array, clone
 
 cimport cython
 
@@ -62,3 +63,20 @@ def cholesky_delete(np.ndarray L, int go_out):
         cholesky_delete_flt(m / sizeof(float),  n, <float *> L.data,  go_out)
     else:
         raise TypeError("unsupported dtype %r." % L.dtype)
+
+
+cdef void _bin_indices(int[:] v, int[:, :] M):
+    cdef int n = v.shape[0]
+    cdef int m = M.shape[0]
+    cdef int[::1] cur_count = clone(array('i'), m, False)
+    for i in range(n):
+        M[v[i], cur_count[v[i]]] = i
+        cur_count[v[i]] += 1
+
+
+def bin_indices(int[:] v):
+    cdef int[::1] class_count
+    class_count = np.bincount(v)
+    M = np.empty((class_count.shape[0], class_count.max()))
+    _bin_indices(v, M)
+    return M
