1e547ead3ec26e3978df836994547ffeb04f04de
Arthur Mensch <arthur.mensch@m4x.org>
2015-12-18 12:32:39 +0100

    Drago

--- sklearn/model_selection/_search.py
+++ sklearn/model_selection/_search.py
@@ -555,7 +555,7 @@ class BaseSearchCV(six.with_metaclass(ABCMeta, BaseEstimator,
         out = Parallel(
             n_jobs=self.n_jobs, verbose=self.verbose,
             max_nbytes=0,
-            mmap_mode='r',
+            mmap_mode='r+',
             pre_dispatch=pre_dispatch
         )(delayed(_fit_and_score)(clone(base_estimator), X, y, self.scorer_,
                                   train, test, self.verbose, parameters,
