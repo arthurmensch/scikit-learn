2287878474e27e96f9e7af7e45db30addab37341
Arthur Mensch <arthur.mensch@m4x.org>
2015-12-27 18:20:48 +0100

    Test frodo

--- sklearn_recommender/examples/dl_bagging.py
+++ sklearn_recommender/examples/dl_bagging.py
@@ -61,7 +61,7 @@ X_csr = X_csr[permutation]
 
 X, y = array_to_fm_format(X_csr)
 
-uniform_split = ShuffleSplit(n_iter=4,
+uniform_split = ShuffleSplit(n_iter=2,
                              test_size=.25, random_state=random_state)
 
 fm_decoder = FMDecoder(n_samples=X_csr.shape[0], n_features=X_csr.shape[1])
@@ -87,7 +87,7 @@ dl_cv = GridSearchCV(dl_rec, param_grid={'alpha': np.logspace(-3, 0, 4),
                      error_score=-1000,
                      memory=mem,
                      n_jobs=30,
-                     refit=True,
+                     refit='bagging',
                      verbose=10)
 
 scores = Parallel(n_jobs=1, verbose=10, max_nbytes='100M')(
