7bd459639b9f271b2948de8e65080b105da664bc
Arthur Mensch <arthur.mensch@m4x.org>
2015-12-29 17:55:49 +0100

    Test AWS

--- sklearn_recommender/examples/probe_dl_recommender.py
+++ sklearn_recommender/examples/probe_dl_recommender.py
@@ -81,8 +81,8 @@ dl_list = [DLRecommender(fm_decoder,
                          l1_ratio=0.,
                          random_state=0)
            for alpha in np.logspace(-3, 0, 4)
-           for learning_rate in np.linspace(.5, .5, 1)
-           for learning_rate_offset in np.logspace(0, 3, 4)
+           for learning_rate in np.linspace(.75, .75, 1)
+           for learning_rate_offset in [0, 10, 100, 1000]
            for n_components in [20]
            for batch_size, decreasing_batch_size in [[4, False]]]
 # dl = DLRecommender(fm_decoder,
