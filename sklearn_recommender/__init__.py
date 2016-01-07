66db834dbc63125f417f0d2d85c131ae4220e5e5
Arthur Mensch <arthur.mensch@m4x.org>
2016-01-07 13:23:11 +0100

    Moved sklearn_recommender

--- sklearn_recommender/__init__.py
+++
@@ -1,8 +0,0 @@
-from .softimpute_als import SoftImputer
-from .base import BaseRecommender
-from .convex_fm import ConvexFM
-from .dl_recommender import DLRecommender
-
-__all__ = ['SoftImputer',
-           'BaseRecommender',
-           'ConvexFM', 'DLRecommender']
--- sklearn_recommender/examples/cluster/simple-launcher/cluster_main.py
+++
@@ -1,9 +0,0 @@
-import getopt
-
-import sys
-
-
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv
-    getopt.getopt(argv, )
